8f2aeb0fda2d1f7774f76590dcc129bcf5f53bdd
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-11-15 14:45:08 +0100

    dsd

--- steg 2/Program.cs
+++ steg 2/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿string[] goobieWoobies= ["Goopu Loopu", "Stoopu Doopu", "TJ kream", "GubGub", "eEern"];
+Console.WriteLine("Top 10 Goobie Woobies:");
+Console.ReadKey();
+foreach (string Woobies in goobieWoobies)
+{
+  Console.WriteLine(Woobies);
+}
+Console.ReadLine();

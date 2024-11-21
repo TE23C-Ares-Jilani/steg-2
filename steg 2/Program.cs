c6272107ebc7a1262850aca63529001eb29fc766
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-11-21 13:03:08 +0100

    igyugy

--- steg 2/Program.cs
+++ steg 2/Program.cs
@@ -1,8 +1,12 @@
-﻿string[] goobieWoobies= ["Goopu Loopu", "Stoopu Doopu", "TJ kream", "GubGub", "eEern"];
-Console.WriteLine("Top 10 Goobie Woobies:");
-Console.ReadKey();
-foreach (string Woobies in goobieWoobies)
+﻿
+string[] leksaker= ["Barbie", "Lego", "Slime", "Nerf", "yo-yo"];
+int[] goobieWoobies= [1, 3, 5, 7, 9];
+string[] names= ["Dac", "Wing", "Brian", "Zen", "Sador"];
+Console.WriteLine("Everyones ratings of different toys:");
+Console.ReadLine();
+for (int i = 0; i < 5; i++ )
 {
-  Console.WriteLine(Woobies);
+  int r = Random.Shared.Next(4);
+  Console.WriteLine($"{names[i]} ger {leksaker[i]} betyget {goobieWoobies[r]}");
 }
 Console.ReadLine();

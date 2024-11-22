2a3f928974c2531a14432db761de0846f36ce52b
Ares Jilani <ares.jilani@elev.ga.ntig>
2024-11-22 14:33:43 +0100

    fsf

--- steg 2/Program.cs
+++ steg 2/Program.cs
@@ -1,12 +1,42 @@
 ﻿
-string[] leksaker= ["Barbie", "Lego", "Slime", "Nerf", "yo-yo"];
-int[] goobieWoobies= [1, 3, 5, 7, 9];
-string[] names= ["Dac", "Wing", "Brian", "Zen", "Sador"];
-Console.WriteLine("Everyones ratings of different toys:");
-Console.ReadLine();
-for (int i = 0; i < 5; i++ )
+// string[] leksaker= ["Barbie", "Lego", "Slime", "Nerf", "yo-yo"];
+// int[] goobieWoobies= [1, 3, 5, 7, 9];
+// string[] names= ["Dac", "Wing", "Brian", "Zen", "Sador"];
+// Console.WriteLine("Everyones ratings of different toys:");
+// Console.ReadLine();
+// for (int i = 0; i < 5; i++ )
+// {
+//   int r = Random.Shared.Next(4);
+//   Console.WriteLine($"{names[i]} ger {leksaker[i]} betyget {goobieWoobies[r]}");
+// }
+// Console.ReadLine();
+using System.Security.Authentication;
+
+List<string> cities = [];
+string city = "";
+
+while (city != "exit")
 {
-  int r = Random.Shared.Next(4);
-  Console.WriteLine($"{names[i]} ger {leksaker[i]} betyget {goobieWoobies[r]}");
+  Console.WriteLine("Type exit at anytime to see the list and quit.");
+  Console.WriteLine("write the name of a city:");
+  city = Console.ReadLine();
+  if (city == "exit")
+  {
+
+  }
+  else
+  {
+  Console.Clear();
+  cities.Add(city);
+  int i = -1;
+  Console.WriteLine("List of cities:");
+  foreach (string citys in cities)
+  {
+    i++;
+    Console.WriteLine(cities[i]);
+    
+  }
+  Console.ReadLine();
+  }
 }
 Console.ReadLine();
